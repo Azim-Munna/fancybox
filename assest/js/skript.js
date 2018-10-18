f4427578466fbca2dc4f4d8c46d93e320ffc996a
Azim Munna <31302195+Azim-Munna@users.noreply.github.com>
2018-10-19 01:36:15 +0600

    dadf

---
+++ assest/js/skript.js
@@ -0,0 +1,30 @@
+(function($) {
+    "use strict"
+
+
+$('[data-fancybox]').fancybox({
+    youtube : {
+        controls : 0,
+        showinfo : 0
+    },
+    vimeo : {
+        color : 'f00'
+    }
+});
+// })(jQuery);
+// jQuery(document).ready(function($) {
+
+//     var menuTrigger = "<span class='caret caret-up'></span>";
+
+//     var hasSubmenu = $('ul li').has('ul').addClass('hasSubmenu').find('>a').prepend(menuTrigger);
+
+//     $('.caret').on('click', function() {
+
+//         $(this).toggleClass('caret-down').closest('li').siblings().find('.caret').removeClass('caret-down');
+
+//         $(this).closest('li').find('>ul').stop(true, true).slideToggle();
+
+
+//     });
+    
+});
